a545e3b7cf69ad724380639af536a9c0259d3c1b
Martín Lombardo <mlombardo9@gmail.com>
2017-07-20 13:51:32 -0300

    Added missing swab function in Android
    Android doesn't provide a swab function.

--- LibRawLite/internal/dcraw_common.cpp
+++ LibRawLite/internal/dcraw_common.cpp
@@ -16,6 +16,27 @@
 
 #line 267 "dcraw/dcraw.c"
 
+// Missing swab function in Android
+#if ANDROID
+	void swab(const void *from, void *to, ssize_t n)
+	{
+		if (n < 0)
+			return;
+
+		for (ssize_t i = 0; i < (n / 2) * 2; i += 2)
+		{
+#ifdef __arch__swab16
+			*((uint16_t*)to + i) = __arch__swab16(*((uint16_t*)from + i));
+#else
+			uint16_t val = *((uint16_t*)from + i);
+			uint16_t hi = (val & 0xFF) << 8;
+			uint16_t lo = (val >> 8) & 0xFF;
+			*((uint16_t*)to + i) = (hi | lo);
+#endif
+		}
+	}
+#endif
+
 #ifndef __GLIBC__
 char *my_memmem (char *haystack, size_t haystacklen,
 	      char *needle, size_t needlelen)
